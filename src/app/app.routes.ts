91f27a9d5597f27961cc84e7e8cce23624e6c750
Ayslla Gomes <ayslla.gomes@outlook.com>
2025-09-04 18:48:56 -0300

    Forçando ir para /demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'demo', pathMatch: 'full' },
+  { path: '', pathMatch: 'full', redirectTo: 'demo' },
   {
     path: 'demo',
     loadComponent: () => import('./demo/demo-page.component').then(m => m.DemoPageComponent)
-  }
+  },
+  { path: '**', redirectTo: 'demo' }
 ];
